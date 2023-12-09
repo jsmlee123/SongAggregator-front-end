4f258140e1f00d946365a4aaf008a2b00ba14004
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2023-12-09 03:10:51 -0500

    search

--- song-aggregator-front-end/src/Search/Results/index.js
+++ song-aggregator-front-end/src/Search/Results/index.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router";
 import SearchBar from "../SearchBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { fetchSong } from "./client";
+import { Link } from "react-router-dom";
 
 
 function Results() {
@@ -22,15 +23,25 @@ function Results() {
     
     return (
         <div className="container d-flex flex-column">
-            <SearchBar search={searchCriteria} callback={getSearchResults} />
-            <div>
-                {Object.keys(tracks).map((key) => {
-                    return (
-                        <div >
-                            {tracks[key].name} {tracks[key].artist}
-                        </div>
-                    );
-                })}
+            <div className="mt-2">
+                <SearchBar search={searchCriteria} callback={getSearchResults} />
+                <div className="d-flex flex-column flex-shrink-1">
+                    {Object.keys(tracks).map((key) => {
+                        return (
+                            <Link
+                                key={tracks[key].name}
+                                to={`/details/${tracks[key].artist}/${tracks[key].name}`}
+                                className="text-decoration-none text-dark"
+                            >
+                                <div className="card rounded-0 mt-1">
+                                    <div class="card-body ">
+                                        {tracks[key].name} - {tracks[key].artist}
+                                    </div>
+                                </div>
+                            </Link>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     )
