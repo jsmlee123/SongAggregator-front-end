df7f532b87aaaae96830f17a30a772f20daa25b7
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2023-12-09 20:39:34 -0500

    details

--- song-aggregator-front-end/src/Details/index.js
+++ song-aggregator-front-end/src/Details/index.js
@@ -1,41 +1,79 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { fetchSong } from "./client";
-import { addSong, findByUserName } from "../GlobalClient";
+import { addSong, findByUserName, findSong } from "../GlobalClient";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 function Details() {
     const { artistName, songName } = useParams();
 
-    const [track, setTrack] = useState({});
     const [artist, setArtist] = useState(null);
-    const [localTrack, setLocalTrack] = useState({});
+
+    const [localTrack, setLocalTrack] = useState({
+        ArtistName: "", 
+        SongName: ""
+    });
 
     
     useEffect(() => {
-        
         findByUserName(artistName)
             .then((user) => setArtist(user))             
             .catch((e) => console.log(e));
-
-        fetchSong(songName, artistName)
-            .then((response) => setTrack(response))
-            .then(() => {
-                const songObj = { ArtistName: artistName, SongName: songName}
-                if (artist) {
-                    songObj["ArtistId"] = artist._id;
+        
+        findSong(artistName, songName)
+            .then((response) => { 
+                if (response) {
+                    setLocalTrack(response);
+                } else {
+                    fetchSong(songName, artistName)
+                    .then((response) => {
+                        const songObj = { ArtistName: artistName, SongName: songName}
+                        if (artist) {
+                            songObj["ArtistId"] = artist._id;
+                        }
+                        console.log(response)
+                        if ("wiki" in response) {
+                            const arefIndex = response.wiki.summary.indexOf("<a href");
+                            songObj["SongDescription"] = response.wiki.summary
+                                .slice(0, arefIndex != -1 ? arefIndex : response.wiki.summary.length);
+                        }
+                        if ("album" in response) {
+                            songObj["ImageURL"] = response.album.image["3"]["#text"];
+                        } 
+                        addSong(songObj)
+                            .then((response) => setLocalTrack(response))
+                            .catch((e) => console.log(e));
+                    });
                 }
-                addSong(songObj)
-                    .then((response) => console.log(response))
-                    .catch((e) => console.log(e));
             });
     }, []);
 
     
     
     return (
-        <div>
-            {track.name}
+        <div className="d-flex flex-row details-page-container">
+            <div className="d-flex flex-column card song-info-card rounded-4 ">
+                {"ImageURL" in localTrack && 
+                    <img src={localTrack.ImageURL}
+                        className="card-img-top" alt="..."></img>
+                }
+                <h4 className="font-weight-bold">
+                    {localTrack.SongName}
+                </h4>
+                <h5>
+                    {localTrack.ArtistName /* conditionally make think link to artist page if possible*/}
+                </h5>
+                <p>
+                    {localTrack.SongDescription}
+                </p>
+            </div>
+
+            <div>
+                
+            </div>
         </div>
+        
     );
 }
 
--- song-aggregator-front-end/src/GlobalClient.js
+++ song-aggregator-front-end/src/GlobalClient.js
@@ -2,9 +2,26 @@ import axios from "axios";
 export const BASE_API = "http://localhost:4000";
 export const USERS_API = `${BASE_API}/api/users`;
 export const SONG_API = `${BASE_API}/api/songs`;
+export const LAST_FM_KEY = "300174397d5c23bd99ff6cf5a31e4b3e"; //fix this
 const request = axios.create({
   withCredentials: true,
 });
+
+export const fetchSongRemote = async (songName, artistName) => {
+  const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${LAST_FM_KEY}&artist=${artistName}&track=${songName}&format=json`);
+  const responseObject = response.json();
+
+  const track = responseObject.then((response) => {
+    return response.track;
+  });
+  
+  return track;
+};
+
+export const findSong = async (artistName, songName) => {
+  const response = await axios.get(`${SONG_API}/${artistName}/${songName}`);
+  return response.data;
+}
 
 export const findByUserName = async (userName) => {
     const response = await axios.get(`${USERS_API}/username/${userName}`);
