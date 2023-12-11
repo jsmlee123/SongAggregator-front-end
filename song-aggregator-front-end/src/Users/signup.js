add35f594a593a42d68942b66d65ca8214b2de4f
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2023-12-11 18:07:32 -0500

    signup styling

--- song-aggregator-front-end/src/Users/signup.js
+++ song-aggregator-front-end/src/Users/signup.js
@@ -27,43 +27,46 @@ function Signup(props) {
 
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      {error && <div className="alert alert-danger">{error.message}</div>}
-      <input
-        type="text"
-        className="form-control w-25"
-        placeholder='username'
-        value={credentials.username}
-        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-      />
-      <input
-        type="password"
-        className="form-control w-25"
-        placeholder='password'
-        value={credentials.password}
-        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-      />
+    <div className='d-flex flex-column align-items-center justify-content-center login-container'>
+      <div className='d-flex flex-column align-items-center justify-content-center login-card rounded-5'>
+        {error && <div className="alert alert-danger">{error.message}</div>}
+        <h1>Sign Up</h1>
+        <input
+          type="text"
+          className="form-control w-75"
+          placeholder='username'
+          value={credentials.username}
+          onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+        />
+        <input
+          type="password"
+          className="form-control w-75 mt-2"
+          placeholder='password'
+          value={credentials.password}
+          onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+        />
+      
+        <h5>Role: </h5>
+        <select
+          className="form-control w-75"
+          value={credentials.role}
+          onChange={(e) => setCredentials({ ...credentials, role: e.target.value })}
+        >
+          <option value="LISTENER">Listener</option>
+          <option value="ARTIST">Artist</option>
+        </select>
 
-      Role: 
-      <select
-        className="form-control w-25"
-        value={credentials.role}
-        onChange={(e) => setCredentials({ ...credentials, role: e.target.value })}
-      >
-        <option value="LISTENER">Listener</option>
-        <option value="ARTIST">Artist</option>
-      </select>
-
-
-
-      <button onClick={signup} className="btn btn-primary" style={{ marginRight: '10px' }}>
-        Sign Up
-      </button>
-      <Link to="/signin" className="btn btn-primary">
-        Sign In
-      </Link>
+        <div className='d-flex flex-row mt-3'>
+          <button onClick={signup} className="btn btn-secondary btn-lg me-4">
+            Sign Up
+          </button>
+          <Link to="/signin" className="btn btn-secondary btn-lg">
+            Sign In
+          </Link>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
